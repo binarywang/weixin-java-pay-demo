b6b15b189343cc0b7cf0c2bf7db27e023d920a4e
Binary Wang <binarywang@gmail.com>
2018-09-15 18:22:51 +0800

    增加代码，可以指定是否使用沙箱环境

--- src/main/java/com/github/binarywang/demo/wx/pay/config/WxPayConfiguration.java
+++ src/main/java/com/github/binarywang/demo/wx/pay/config/WxPayConfiguration.java
@@ -37,6 +37,9 @@ public class WxPayConfiguration {
     payConfig.setSubMchId(StringUtils.trimToNull(this.properties.getSubMchId()));
     payConfig.setKeyPath(StringUtils.trimToNull(this.properties.getKeyPath()));
 
+    // 可以指定是否使用沙箱环境
+    payConfig.setUseSandboxEnv(false);
+
     WxPayService wxPayService = new WxPayServiceImpl();
     wxPayService.setConfig(payConfig);
     return wxPayService;
