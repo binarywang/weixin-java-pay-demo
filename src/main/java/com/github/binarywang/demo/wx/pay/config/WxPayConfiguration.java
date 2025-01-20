5b935bf84cccc5be14a27788f26b2ccdbe04c471
nadirvishun <nadirvishun@gmail.com>
2025-01-20 14:02:31 +0800

    优化多商户配置，增加v3版本部分配置和示例 (#133)

--- src/main/java/com/github/binarywang/demo/wx/pay/config/WxPayConfiguration.java
+++ src/main/java/com/github/binarywang/demo/wx/pay/config/WxPayConfiguration.java
@@ -1,15 +1,20 @@
 package com.github.binarywang.demo.wx.pay.config;
 
+import com.github.binarywang.demo.wx.pay.config.WxPayProperties.Config;
 import com.github.binarywang.wxpay.config.WxPayConfig;
 import com.github.binarywang.wxpay.service.WxPayService;
 import com.github.binarywang.wxpay.service.impl.WxPayServiceImpl;
 import lombok.AllArgsConstructor;
+import me.chanjar.weixin.common.error.WxRuntimeException;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author Binary Wang
@@ -21,28 +26,34 @@ import org.springframework.context.annotation.Configuration;
 public class WxPayConfiguration {
   private WxPayProperties properties;
 
-  @Bean(name = "wx2Service")
-  @ConditionalOnMissingBean(name = "wx2Service")
-  public WxPayService wx2Service() {
-    return new WxPayServiceImpl();
-  }
-
-  @Bean(name = "wxService")
-  @ConditionalOnMissingBean(name = "wxService")
+  @Bean
+  @ConditionalOnMissingBean
   public WxPayService wxService() {
-    WxPayConfig payConfig = new WxPayConfig();
-    payConfig.setAppId(StringUtils.trimToNull(this.properties.getAppId()));
-    payConfig.setMchId(StringUtils.trimToNull(this.properties.getMchId()));
-    payConfig.setMchKey(StringUtils.trimToNull(this.properties.getMchKey()));
-    payConfig.setSubAppId(StringUtils.trimToNull(this.properties.getSubAppId()));
-    payConfig.setSubMchId(StringUtils.trimToNull(this.properties.getSubMchId()));
-    payConfig.setKeyPath(StringUtils.trimToNull(this.properties.getKeyPath()));
-
-    // 可以指定是否使用沙箱环境
-    payConfig.setUseSandboxEnv(false);
+    List<Config> configs = this.properties.getConfigs();
+    if (configs == null) {
+      throw new WxRuntimeException("先添加下相关配置，注意别配错了！");
+    }
 
     WxPayService wxPayService = new WxPayServiceImpl();
-    wxPayService.setConfig(payConfig);
+    wxPayService.setMultiConfig(
+      configs.stream().map(a -> {
+        WxPayConfig payConfig = new WxPayConfig();
+        payConfig.setAppId(StringUtils.trimToNull(a.getAppId()));//V3商户模式需要
+        payConfig.setMchId(StringUtils.trimToNull(a.getMchId()));//V3商户模式需要
+        payConfig.setMchKey(StringUtils.trimToNull(a.getMchKey()));
+        payConfig.setSubAppId(StringUtils.trimToNull(a.getSubAppId()));
+        payConfig.setSubMchId(StringUtils.trimToNull(a.getSubMchId()));
+        payConfig.setKeyPath(StringUtils.trimToNull(a.getKeyPath()));
+        payConfig.setApiV3Key(StringUtils.trimToNull(a.getApiV3Key()));//V3商户模式需要
+        payConfig.setCertSerialNo(StringUtils.trimToNull(a.getCertSerialNo()));//V3商户模式需要
+        payConfig.setPrivateCertPath(StringUtils.trimToNull(a.getPrivateCertPath()));//V3商户模式需要
+        payConfig.setPrivateKeyPath(StringUtils.trimToNull(a.getPrivateKeyPath()));//V3商户模式需要
+        payConfig.setPublicKeyPath(StringUtils.trimToNull(a.getPublicKeyPath()));//V3商户模式需要
+        payConfig.setPublicKeyId(StringUtils.trimToNull(a.getPublicKeyId()));//V3商户模式需要
+        // 可以指定是否使用沙箱环境
+        payConfig.setUseSandboxEnv(false);
+        return payConfig;
+      }).collect(Collectors.toMap(WxPayConfig::getMchId, a -> a, (o, n) -> o)));
     return wxPayService;
   }
 
--- src/main/java/com/github/binarywang/demo/wx/pay/config/WxPayProperties.java
+++ src/main/java/com/github/binarywang/demo/wx/pay/config/WxPayProperties.java
@@ -3,6 +3,8 @@ package com.github.binarywang.demo.wx.pay.config;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.List;
+
 /**
  * wxpay pay properties.
  *
@@ -11,34 +13,77 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @Data
 @ConfigurationProperties(prefix = "wx.pay")
 public class WxPayProperties {
-  /**
-   * 设置微信公众号或者小程序等的appid
-   */
-  private String appId;
+  private List<Config> configs;
 
-  /**
-   * 微信支付商户号
-   */
-  private String mchId;
+  @Data
+  public static class Config {
+    /**
+     * 设置微信公众号或者小程序等的appid
+     * (V3商户模式需要)
+     */
+    private String appId;
 
-  /**
-   * 微信支付商户密钥
-   */
-  private String mchKey;
+    /**
+     * 微信支付商户号
+     * (V3商户模式需要)
+     */
+    private String mchId;
 
-  /**
-   * 服务商模式下的子商户公众账号ID，普通模式请不要配置，请在配置文件中将对应项删除
-   */
-  private String subAppId;
+    /**
+     * 微信支付商户密钥
+     */
+    private String mchKey;
 
-  /**
-   * 服务商模式下的子商户号，普通模式请不要配置，最好是请在配置文件中将对应项删除
-   */
-  private String subMchId;
+    /**
+     * 服务商模式下的子商户公众账号ID，普通模式请不要配置，请在配置文件中将对应项删除
+     */
+    private String subAppId;
 
-  /**
-   * apiclient_cert.p12文件的绝对路径，或者如果放在项目中，请以classpath:开头指定
-   */
-  private String keyPath;
+    /**
+     * 服务商模式下的子商户号，普通模式请不要配置，最好是请在配置文件中将对应项删除
+     */
+    private String subMchId;
+
+    /**
+     * apiclient_cert.p12文件的绝对路径，或者如果放在项目中，请以classpath:开头指定
+     */
+    private String keyPath;
+
+    /**
+     * apiV3 秘钥值
+     * (V3商户模式需要)
+     */
+    private String apiV3Key;
+
+    /**
+     * apiV3 证书序列号值
+     * (V3商户模式需要)
+     */
+    private String certSerialNo;
+
+    /**
+     * apiclient_cert.pem证书文件的绝对路径或者以classpath:开头的类路径.
+     * (V3商户模式需要，这里用的是文件路径，可以用其它base64编码或字节数组参数替代)
+     */
+    private String privateCertPath;
+
+    /**
+     * apiclient_key.pem证书文件的绝对路径或者以classpath:开头的类路径.
+     * (V3商户模式需要，这里用的是文件路径，可以用其它base64编码或字节数组参数替代)
+     */
+    private String privateKeyPath;
+
+    /**
+     * 微信支付公钥，pub_key.pem证书文件的绝对路径或者以classpath:开头的类路径.
+     * (V3商户模式需要，这里用的是文件路径，可以用其它base64编码或字节数组参数替代，2024.08后的新商户验签需要用此公钥)
+     */
+    private String publicKeyPath;
+
+    /**
+     * 微信支付公钥ID
+     * (V3商户模式需要，2024.08后的新商户验签需要用此公钥ID)
+     */
+    private String publicKeyId;
+  }
 
 }
